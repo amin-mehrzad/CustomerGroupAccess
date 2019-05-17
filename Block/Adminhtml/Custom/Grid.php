3f73132088a53bbc05f6a96b3a03139ffce77089
Amin Mehrzad <amin.mehrzad@gmail.com>
2019-05-17 16:08:51 -0700

    fixedbug - get username

--- Block/Adminhtml/Custom/Grid.php
+++ Block/Adminhtml/Custom/Grid.php
@@ -1,26 +1,65 @@
 <?php
 
 namespace XCode\CustomerGroupAccess\Block\Adminhtml\Custom;
+
 use Magento\Backend\Block\Widget\Grid as WidgetGrid;
+use \Magento\Backend\Helper\Data;
+use \Magento\Framework\View\Element\BlockFactory;
+
+//use Magento\Backend\Block\Widget\Context ;
 class Grid extends WidgetGrid
-    {
-         protected function _construct()
-         {
-             parent::_construct();
-             $this->setSaveParametersInSession(true);        
-             //for default filter
-             if ($this->hasData('default_filter')){
-                 $this->setDefaultFilter($this->getData('default_filter'));
-             }
-         }
-         protected function _prepareCollection()
-         {
-             //on clicking reset filter on Grid it will make 'complete' status as default:
-              if(!$this->getParam($this->getVarNameFilter(), null)) {
-                  $this->getCollection()->addFieldToFilter('customer_group', array('in' => ['premiumbuyer','wholesale']));
-                  $data['customer_group'] = 'premiumbuyer';           
-                  $this->_setFilterValues($data);
-              }
-              parent::_prepareCollection();
-         }
+{
+    protected $_blockFactory;
+    protected $_backendHelper;
+    protected $authSession;
+
+    public function __construct(
+        \Magento\Framework\View\Element\BlockFactory $blockFactory,
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Backend\Helper\Data $backendHelper,
+        \Magento\Backend\Model\Auth\Session $authSession,
+        array $data = []
+    ) {
+
+        $this->_backendHelper = $backendHelper;
+        $this->_blockFactory = $blockFactory;
+        $this->authSession = $authSession;
+        parent::__construct($context, $backendHelper, $data);
+        //      $this->setSaveParametersInSession(true);
+        //      //for default filter
+        //      if ($this->hasData('default_filter')){
+        //          $this->setDefaultFilter($this->getData('default_filter'));
+        //      }
     }
+
+    public function getCurrentUser()
+    {
+        return $this->authSession->getUser();
+    }
+
+    protected function _prepareCollection()
+    {
+
+        echo $this->getNameInLayout() . "<br>";
+        $layoutName = $this->getNameInLayout();
+        if ($layoutName == "adminhtml.customer.grid.container") {
+            $v = $this->getParam($this->getVarNameFilter(), null);
+            echo $this->getVarNameFilter() . "<br>";
+
+            $user = $this->getCurrentUser();
+
+            if ($user->getUsername() == "amin") {
+                // echo $user;
+                print_r($user->getUsername());
+                //on clicking reset filter on Grid it will make 'complete' status as default:
+
+                if (!$this->getParam($this->getVarNameFilter(), null)) {
+                    $this->getCollection()->addFieldToFilter('customer_group', array('in' => ['wholesale']));
+                    //$data['customer_group'] = 'wholesale';
+                    // $this->_setFilterValues($data);
+                }
+            }
+        }
+        parent::_prepareCollection();
+    }
+}
--- Model/ResourceModel/Order/Customer/Collection.php
+++ Model/ResourceModel/Order/Customer/Collection.php
@@ -1,8 +1,19 @@
 <?php
 namespace XCode\CustomerGroupAccess\Model\ResourceModel\Order\Customer;
 
+//se Magento\Backend\Block\Widget\Grid as WidgetGrid;
+
 class Collection extends \Magento\Sales\Model\ResourceModel\Order\Customer\Collection
 {
+    // protected $widgetGrid;
+
+    // public function __construct(
+    //     WidgetGrid $widgetGrid
+    // ){
+    //     $this->widgetGrid;
+    //     parent::__construct();
+    // }
+
     protected function _initSelect()
     {
         parent::_initSelect();
@@ -18,4 +29,15 @@ class Collection extends \Magento\Sales\Model\ResourceModel\Order\Customer\Colle
 
         return $this;
     }
+
+    // protected function _prepareCollection()
+    // {
+    //     //on clicking reset filter on Grid it will make 'complete' status as default:
+    //      if(!$this->getParam($this->getVarNameFilter(), null)) {
+    //          $this->getCollection()->addFieldToFilter('customer_group', array('in' => ['premiumbuyer','wholesale']));
+    //        //   $data['customer_group'] = 'premiumbuyer';
+    //        //   $this->_setFilterValues($data);
+    //      }
+    //      parent::_prepareCollection();
+    // }
 }
